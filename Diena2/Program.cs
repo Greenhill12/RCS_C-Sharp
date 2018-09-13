5ad11026999d44237a242f329f94920b3913bbbb
Janis <johnnydille@inbox.lv>
2018-09-13 22:34:59 +0300

    Day2

---
+++ Diena2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diena2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Piemers();
+            //Vecums();
+            //SkaitluSalidzinasana();
+            MenesuDienas();
+
+            Console.ReadLine();
+        }
+
+        static void Vecums()
+        {
+            int vecums = AtgriezVecumu();
+
+            if (vecums >= 18)
+            {
+                Console.Write("Tavs vecums ir vismaz 18 gadi");
+            }
+            else
+            {
+                Console.Write("Tavs vecums nav vēl 18 gadi");
+            }
+        }
+
+        static int AtgriezVecumu()
+        {
+            Console.Write("Ievadi savu vecumu:");
+            int vecums = Convert.ToInt32(Console.ReadLine());
+            return vecums;
+        }
+        
+
+
+        static void SkaitluSalidzinasana()
+        {
+            int skaitlis1 = AtgriezSkaitli1();
+            int skaitlis2 = AtgriezSkaitli2();
+
+            if (skaitlis1 == skaitlis2)
+            {
+                Console.Write("Skaitļi ir vienādi");
+            }
+            else
+            {
+                Console.Write("Skaitļi nav vienādi");
+            }
+        }
+
+        static int AtgriezSkaitli1()
+        {
+            Console.Write("Ievadi skaitli Nr.1: ");
+            int skaitlis1 = Convert.ToInt32(Console.ReadLine());
+            return skaitlis1;
+        }
+
+        static int AtgriezSkaitli2()
+        {
+            Console.Write("Ievadi skaitli Nr.2: ");
+            int skaitlis2 = Convert.ToInt32(Console.ReadLine());
+            return skaitlis2;
+        }
+
+
+        static void MenesuDienas()
+        {
+            Console.Write("Ievadi mēneša pirmos trīs burtus: ");
+            string menesis = Console.ReadLine().ToLower();
+
+            if (menesis == "feb")
+            {
+                Console.Write("Mēnesī ir 28 dienas. Ja ir garais gads, tad 29 dienas");
+            }
+            else if (menesis == "apr" || menesis == "jun" || menesis == "sep" || menesis == "nov")
+            {
+                Console.Write("Mēnesī ir 30 dienas");
+            }
+            else if (menesis == "jan" || menesis == "mar" || menesis == "mai" || menesis == "jul" || menesis == "aug" || menesis == "okt" || menesis == "dec")
+            {
+                Console.Write("Mēnesī ir 30 dienas");
+            }
+            else
+            {
+                Console.Write("Ievadītais neatbilst prasītajam");
+            }
+        }
+
+
+
+        static void Piemers()
+        {
+            int skaitlis = AtgriezSkaitli();
+
+            if(skaitlis > 10)
+            {
+                Console.Write("Skaitlis ir lielāks par 10");
+            }
+            else if(skaitlis == 10)
+            {
+                Console.Write("Skaitlis ir vienāds ar 10");
+            }
+            else if (skaitlis == 9)
+            {
+                Console.Write("Skaitlis ir vienāds ar 9");
+            }
+            else
+            {
+                Console.Write("Skaitlis ir mazāks par 10");
+            }
+        }
+
+        static int AtgriezSkaitli()
+        {
+            return 5;   
+        }
+    }
+}
---
+++ Diena2_Uzdevumi/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diena2_Uzdevumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AttalumaAprekins();
+            //LietotajaDarbibas();
+            LietotajaGradi();
+        }
+
+        static void AttalumaAprekins()
+        {
+            Console.Write("Ievadi attālumu metros: ");
+            int attalums = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ievadi patērētās stundas: ");
+            int stundas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ievadi patērētās minūtes: ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ievadi paterētās sekundes: ");
+            int sekundes = Convert.ToInt32(Console.ReadLine());
+
+            double laiksKopa = sekundes + minutes * 60 + stundas * 60 * 60;
+            double atrums = attalums / laiksKopa;
+            double atrumsKm = (attalums / 1000) / (laiksKopa / 3600);
+            double attalumsKm = attalums / 1000;
+            double attalumsJudz = attalumsKm * 0.621371;
+
+            Console.WriteLine();
+            Console.WriteLine("Kopā pavadītais laiks ir " + laiksKopa + " sekundes");
+            Console.WriteLine("Ātrums ir " + atrums + " m/s");
+            Console.WriteLine("Ātrums ir " + atrumsKm + " km/h");
+            Console.WriteLine("Attālums ir " + attalumsKm + " km");
+            Console.WriteLine("Attālums ir " + attalumsJudz + " jūdzes");
+
+            Console.ReadLine();
+        }
+
+        static void LietotajaDarbibas()
+        {
+           Console.Write("Ievadi skaitli Nr.1: ");
+           double skaitlis1 = Convert.ToDouble(Console.ReadLine());
+
+           Console.Write("Ievadi skaitli Nr.2: ");
+           double skaitlis2 = Convert.ToDouble(Console.ReadLine());
+
+           Console.Write("Ievadi darbību, kuru vēlies veikt ar skaitļiem: ");
+           string darbiba = Console.ReadLine();
+                    
+           if (darbiba == "+")
+            {
+                double summa = skaitlis1 + skaitlis2;
+                Console.WriteLine("Skaitļu summa ir " + summa);
+            }
+            else if (darbiba == "-")
+            {
+                double starpiba = skaitlis1 - skaitlis2;
+                Console.WriteLine("Skaitļu starpība ir " + starpiba);
+            }
+            else if (darbiba == "*")
+            {
+                double reizinajums = skaitlis1 * skaitlis2;
+                Console.WriteLine("Skaitļu reizinājums ir " + reizinajums);
+            }
+            else if (darbiba == "/")
+            {
+                double dalijums = skaitlis1 / skaitlis2;
+                Console.WriteLine("Skaitļu dalījums ir " + dalijums);
+            }
+            else
+            {
+                Console.WriteLine("Rezultātu nav iespējams aprēķināt");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void LietotajaGradi()
+        {
+            Console.Write("Ievadi temperatūru: ");
+            double gradi = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ievadi esošās temperatūras mērvienību (C, F, K): ");
+            string mervienibaIn = Console.ReadLine().ToUpper();
+
+            Console.Write("Ievadi vajadzīgo temperatūras mērvienību (C, F, K): ");
+            string mervienibaOut = Console.ReadLine().ToUpper();
+
+            if ((mervienibaIn != "C" && mervienibaIn != "F" && mervienibaIn != "K") || (mervienibaOut != "C" && mervienibaOut != "F" && mervienibaOut != "K"))
+            {
+                Console.WriteLine("Kļūda ievadē!");
+            }
+            else if (mervienibaIn == mervienibaOut)
+            {
+                Console.WriteLine("Temperatūra ir tik cik ievadīji");
+            }
+            else if (mervienibaIn == "C")
+            {
+                if (mervienibaOut == "F")
+                {
+                    double CtoF = (gradi * 1.8) + 32;
+                    Console.WriteLine("Temperatūra ir " + CtoF + " fārenheiti");
+                }
+                else if (mervienibaOut == "K")
+                {
+                    double CtoK = gradi + 273.15;
+                    Console.WriteLine("Temperatūra ir " + CtoK + " kelvini");
+                }
+            }
+            else if (mervienibaIn == "F")
+            {
+                if (mervienibaOut == "C")
+                {
+                    double FtoC = (gradi - 32) / 1.8;
+                    Console.WriteLine("Temperatūra ir " + FtoC + " celsija grādi");
+                }
+                else if (mervienibaOut == "K")
+                {
+                    double FtoK = (gradi + 459.67) * 1.8;
+                    Console.WriteLine("Temperatūra ir " + FtoK + " kelvini");
+                }
+            }
+            else if (mervienibaIn == "K")
+            {
+                if (mervienibaOut == "C")
+                {
+                    double KtoC = gradi - 273.15;
+                    Console.WriteLine("Temperatūra ir " + KtoC + " celsija grādi");
+                }
+                else if (mervienibaOut == "F")
+                {
+                    double KtoF = (gradi * 1.8) - 459.67;
+                    Console.WriteLine("Temperatūra ir " + KtoF + " fārenheiti");
+                }
+                else if (mervienibaOut == "K")
+                {
+                    Console.WriteLine("Temperatūra ir " + gradi + " kelvini");
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
