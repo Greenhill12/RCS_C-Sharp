a61c8affbfc1762b1b7279572793978e87c8eb00
Janis <johnnydille@inbox.lv>
2018-09-13 22:34:19 +0300

    Day1

---
+++ Diena-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diena1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string izvadamsTeksts = Teksts();
+            izvadamsTeksts = izvadamsTeksts + " " + Skaitlis();
+
+            Console.WriteLine(izvadamsTeksts);
+            BezRezultata();
+
+            // sagaidīt lietotāja ievadi
+            Console.ReadLine();
+        }
+
+        static void BezRezultata()
+        {
+            Console.WriteLine("Izsaukta funkcija BezRezultata");
+        }
+
+        static string Teksts()
+        {
+            return "Hello world!";
+        }
+
+        static int Skaitlis()
+        {
+            return 5;
+        }
+    }
+}
